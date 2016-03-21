48621b5d1305069d65e6eab2470f6e3d34f88a1f
root <root@localhost.localdomain>
2016-03-21 03:05:17 +0000

    first commit

---
+++ database/migrations/2016_03_19_062608_add_bookdata_to_notes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBookdataToNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->string('title');
+            $table->string('author');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            //
+        });
+    }
+}
